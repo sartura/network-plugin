b4a744c72feeaaa18ec30ac3efbd1893e796a4b0
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-07-31 14:12:03 +0200

    add cmake variable PLUGIN
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- src/terastream.c
+++ src/terastream.c
@@ -264,3 +264,52 @@ sr_plugin_cleanup_cb(sr_session_ctx_t *session, void *private_ctx)
 
     SRP_LOG_DBG_MSG("Plugin cleaned-up successfully");
 }
+
+#ifndef PLUGIN
+#include <signal.h>
+#include <unistd.h>
+
+volatile int exit_application = 0;
+
+static void
+sigint_handler(__attribute__((unused)) int signum) {
+	INF_MSG("Sigint called, exiting...");
+	exit_application = 1;
+}
+
+int
+main() {
+	INF_MSG("Plugin application mode initialized");
+	sr_conn_ctx_t *connection = NULL;
+	sr_session_ctx_t *session = NULL;
+	void *private_ctx = NULL;
+	int rc = SR_ERR_OK;
+
+	/* connect to sysrepo */
+	rc = sr_connect(YANG_MODEL, SR_CONN_DEFAULT, &connection);
+	CHECK_RET(rc, cleanup, "Error by sr_connect: %s", sr_strerror(rc));
+
+	/* start session */
+	rc = sr_session_start(connection, SR_DS_RUNNING, SR_SESS_DEFAULT, &session);
+	CHECK_RET(rc, cleanup, "Error by sr_session_start: %s", sr_strerror(rc));
+
+	rc = sr_plugin_init_cb(session, &private_ctx);
+	CHECK_RET(rc, cleanup, "Error by sr_plugin_init_cb: %s", sr_strerror(rc));
+
+	/* loop until ctrl-c is pressed / SIGINT is received */
+	signal(SIGINT, sigint_handler);
+	signal(SIGPIPE, SIG_IGN);
+	while (!exit_application) {
+		sleep(1);  /* or do some more useful work... */
+	}
+
+	sr_plugin_cleanup_cb(session, private_ctx);
+cleanup:
+	if (NULL != session) {
+		sr_session_stop(session);
+	}
+	if (NULL != connection) {
+		sr_disconnect(connection);
+	}
+}
+#endif
