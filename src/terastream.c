79dfb4763bc9e6b48b154cf3010169621831dd04
Luka Paulic <luka.paulic0@gmail.com>
2019-09-26 14:38:48 +0200

    Update sysrepo API

--- src/terastream.c
+++ src/terastream.c
@@ -451,7 +451,7 @@ static int parse_network_config(sr_ctx_t *ctx) {
   }
 
   INF_MSG("apply changes the sysrepo changes");
-  rc = sr_apply_changes(ctx->startup_sess);
+  rc = sr_apply_changes(ctx->startup_sess, 0);
   CHECK_RET(rc, error, "Couldn't apply changes for initial interfaces: %s",
             sr_strerror(rc));
 
@@ -510,7 +510,7 @@ static int module_change_cb(sr_session_ctx_t *session, const char *module_name,
     /* copy running datastore to startup */
 
     rc = sr_copy_config(ctx->startup_sess, module_name, SR_DS_RUNNING,
-                        SR_DS_STARTUP);
+                        SR_DS_STARTUP, 0);
     if (SR_ERR_OK != rc) {
       WRN_MSG("Failed to copy running datastore to startup");
       /* TODO handle this error */
@@ -778,7 +778,7 @@ int sr_plugin_init_cb(sr_session_ctx_t *session, void **private_ctx) {
   CHECK_RET(rc, error, "Couldn't initialize datastores: %s", sr_strerror(rc));
 
   rc = sr_copy_config(ctx->startup_sess, YANG_MODEL, SR_DS_STARTUP,
-                      SR_DS_RUNNING);
+                      SR_DS_RUNNING, 0);
   if (SR_ERR_OK != rc) {
     WRN_MSG("Failed to copy running datastore to startup");
     /* TODO handle this error */
