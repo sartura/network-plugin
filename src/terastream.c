aef34621abff657d3b4a9c4993c5a4d25f932e89
Luka Paulic <luka.paulic0@gmail.com>
2020-05-20 13:10:08 +0200

    terastream.c: update Sysrepo API in code

--- src/terastream.c
+++ src/terastream.c
@@ -514,7 +514,7 @@ static int module_change_cb(sr_session_ctx_t *session, const char *module_name,
     /* copy running datastore to startup */
 
     rc = sr_copy_config(ctx->startup_sess, module_name, SR_DS_RUNNING,
-                        SR_DS_STARTUP, 0);
+                        0, 0);
     if (SR_ERR_OK != rc) {
       WRN_MSG("Failed to copy running datastore to startup");
       /* TODO handle this error */
@@ -781,8 +781,8 @@ int sr_plugin_init_cb(sr_session_ctx_t *session, void **private_ctx) {
   rc = sync_datastore(ctx);
   CHECK_RET(rc, error, "Couldn't initialize datastores: %s", sr_strerror(rc));
 
-  rc = sr_copy_config(ctx->startup_sess, YANG_MODEL, SR_DS_STARTUP,
-                      SR_DS_RUNNING, 0);
+  rc = sr_copy_config(ctx->sess, YANG_MODEL, SR_DS_STARTUP,
+                      0, 0);
   if (SR_ERR_OK != rc) {
     WRN_MSG("Failed to copy running datastore to startup");
     /* TODO handle this error */
